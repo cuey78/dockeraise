379c5016aa2f4b35e9c2861865a496977d2bc893
cuey78 <101441768+cuey78@users.noreply.github.com>
2024-06-22 22:35:03 +1000

    Update dockeraise.sh

--- dockeraise.sh
+++ dockeraise.sh
@@ -5,84 +5,154 @@ msg="\033[1;32m[+]\033[m"
 info="\033[0;36m[:]\033[m"
 ask="\033[0;35m[?]\033[m"
 
-# Make sure the script is run as root
-if [[ $EUID -ne 0 ]]; then
-    echo -e "${err} This script must be run as root."
-    exit 1
-fi
+# Check if the machine is running a Debian-based system
+is_debian() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        if [[ "$ID" == "debian" || "$ID_LIKE" =~ "debian" ]]; then
+            return 0
+        fi
+    fi
+    return 1
+}
 
-# Check if Docker is already installed
-if command -v docker &>/dev/null; then
-    while true; do
-        echo -e "${err} Docker is already installed."
-        read -p "Do you want still want to run the script? [y/n] " yn </dev/tty
-        case $yn in
-            [Yy] ) break;;
-            [Nn] ) exit 1;;
-            * ) ;;
-        esac
-    done
-fi
+# Check if Docker is installed
+is_docker_installed() {
+    if command -v docker &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# Check if machine runs Debian (only checks if apt-get exists, maybe needs a better implementation?)
-if ! command -v apt-get &>/dev/null; then
-    echo -e "${err} This script only works on Debian machines, sorry!"
-    exit 1
-fi
+# Ensure the script is run as root
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${err} This script must be run as root."
+        exit 1
+    fi
+}
 
 # Remove old and incompatible packages
-echo -e "${info} Removing old and incompatible packages..."
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove -y $pkg >/dev/null; done
+remove_old_packages() {
+    echo -e "${info} Removing old and incompatible packages..."
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
+        apt-get remove -y $pkg >/dev/null
+    done
+}
 
 # Install dependencies
-echo -e "${info} Updating repositories..."
-apt-get update >/dev/null
-echo -e "${info} Installing dependencies..."
-apt-get install -y ca-certificates curl gnupg >/dev/null
+install_dependencies() {
+    echo -e "${info} Updating repositories..."
+    apt-get update >/dev/null
+    echo -e "${info} Installing dependencies..."
+    apt-get install -y ca-certificates curl gnupg >/dev/null
+}
 
-# Add GPG key
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg &&
-chmod a+r /etc/apt/keyrings/docker.gpg &&
-echo -e "${msg} Added Docker's GPG key"
+# Add Docker GPG key
+add_gpg_key() {
+    install -m 0755 -d /etc/apt/keyrings
+    if curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg; then
+        chmod a+r /etc/apt/keyrings/docker.gpg
+        echo -e "${msg} Added Docker's GPG key"
+    else
+        echo -e "${err} Failed to add Docker's GPG key"
+        exit 1
+    fi
+}
 
-# Add repository
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null &&
-echo -e "${msg} Added Docker's stable repository"
+# Add Docker repository
+add_repository() {
+    if echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null; then
+        echo -e "${msg} Added Docker's stable repository"
+    else
+        echo -e "${err} Failed to add Docker's stable repository"
+        exit 1
+    fi
+}
 
 # Install Docker
-echo -e "${info} Updating repositories..."
-apt-get update >/dev/null
-echo -e "${info} Installing Docker and Docker compose (this might take a while)..."
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null &&
-echo -e "${msg} Docker successfully installed: \033[0;32m$(docker --version)\033[m"
+install_docker() {
+    echo -e "${info} Updating repositories..."
+    apt-get update >/dev/null
+    echo -e "${info} Installing Docker and Docker compose (this might take a while)..."
+    if apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null; then
+        echo -e "${msg} Docker successfully installed: \033[0;32m$(docker --version)\033[m"
+    else
+        echo -e "${err} Docker installation failed"
+        exit 1
+    fi
+}
 
 # Create Docker user
-while true; do
-    echo -e -n "${ask} "
-    read -p "Do you want to create a Docker user? [y/n] " yn </dev/tty
-    case $yn in
-        [Yy] ) while true; do
-            echo -e -n "${ask} "
-            read -p "Please choose an ID for the new user/group: " id </dev/tty;
-            if id $id &>/dev/null; then
-                echo -e "${err} An user with the same ID already exists: \033[0;36m$(id $id)\033[m";
-                continue;
-            else
-                /usr/sbin/groupadd -g $id dockeruser && /usr/sbin/useradd dockeruser -u $id -g $id -m -s /bin/bash && 
-                echo -e "${msg} Docker user created: \033[0;32m$(id dockeruser)\033[m"
-                break;
-            fi
-        done
-        break;;
-        [Nn] ) break;;
-        * ) ;;
-    esac
-done
+create_docker_user() {
+    while true; do
+        echo -e -n "${ask} "
+        read -p "Do you want to create a Docker user? [y/n] " yn </dev/tty
+        case $yn in
+            [Yy] )
+                while true; do
+                    echo -e -n "${ask} "
+                    read -p "Please choose an ID for the new user/group: " id </dev/tty
+                    if id $id &>/dev/null; then
+                        echo -e "${err} A user with the same ID already exists: \033[0;36m$(id $id)\033[m"
+                    else
+                        if /usr/sbin/groupadd -g $id dockeruser && /usr/sbin/useradd dockeruser -u $id -g $id -m -s /bin/bash; then
+                            echo -e "${msg} Docker user created: \033[0;32m$(id dockeruser)\033[m"
+                            break
+                        else
+                            echo -e "${err} Failed to create Docker user"
+                            exit 1
+                        fi
+                    fi
+                done
+                break
+                ;;
+            [Nn] ) break ;;
+            * ) echo "Please answer yes [y] or no [n].";;
+        esac
+    done
+}
 
 # Enable Docker service at startup
-echo -e "${info} Starting Docker services..."
-systemctl enable --now docker.service containerd.service >/dev/null &&
-echo -e "${msg} Docker services started and enabled." || echo -e "${err} Could not start docker services. Try running \033[0;36msystemctl enable --now docker.service containerd.service\033[m again in a few minutes."
+enable_docker_service() {
+    echo -e "${info} Starting Docker services..."
+    if systemctl enable --now docker.service containerd.service >/dev/null; then
+        echo -e "${msg} Docker services started and enabled."
+    else
+        echo -e "${err} Could not start Docker services. Try running \033[0;36msystemctl enable --now docker.service containerd.service\033[m again in a few minutes."
+    fi
+}
 
-echo -e "${info} Process completed. Run '\033[0;36msystemctl status docker\033[m' to check Docker's status."
+main() {
+    check_root
+
+    if is_docker_installed; then
+        while true; do
+            echo -e "${err} Docker is already installed."
+            read -p "Do you still want to run the script? [y/n] " yn </dev/tty
+            case $yn in
+                [Yy] ) break;;
+                [Nn] ) exit 1;;
+                * ) echo "Please answer yes [y] or no [n].";;
+            esac
+        done
+    fi
+
+    if ! is_debian; then
+        echo -e "${err} This script only works on Debian machines, sorry!"
+        exit 1
+    fi
+
+    remove_old_packages
+    install_dependencies
+    add_gpg_key
+    add_repository
+    install_docker
+    create_docker_user
+    enable_docker_service
+
+    echo -e "${info} Process completed. Run '\033[0;36msystemctl status docker\033[m' to check Docker's status."
+}
+
+main "$@"
